34b142d366c5efb8dfb7e7e3a170b6d1960d9b33
AzizTurki07 <torkich7@gmail.com>
2026-02-01 01:56:27 +0100

    Initial commit: Angular + Spring Boot + Docker

---
+++ backend/src/main/java/tn/esprit/nephrobackend/NephroBackendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.nephrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NephroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NephroBackendApplication.class, args);
+    }
+
+}
